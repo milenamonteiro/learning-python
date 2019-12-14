95226f04ddacfb4636e05a8b31be7d828d8faa7f
Milena Monteiro <milenamonteiro2002@hotmail.com>
2019-12-13 22:37:39 -0300

    Upload

---
+++ printformat.py
@@ -0,0 +1,7 @@
+'''Formatting a print statement with variables'''
+CHARACTER_NAME = "George"
+CHARACTER_AGE = 70
+print('There once was a man named {0},'.format(CHARACTER_NAME))
+print('he was {0} years old.'.format(CHARACTER_AGE))
+print('He really liked the name {0}'.format(CHARACTER_NAME))
+print("but didn't like being {0}".format(CHARACTER_AGE))
---
+++ radiuscircle.py
@@ -0,0 +1,5 @@
+import math
+
+RADIUS = float(input("What's the radius? "))
+
+print("The area is {0}".format(math.pi * math.pow(RADIUS, 2)))
